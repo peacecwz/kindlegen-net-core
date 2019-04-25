5952c0868ee50cdef396f8936259ed50481a81ca
Baris Ceviz (peacecwz) <me@barisceviz.com>
2019-04-25 05:38:05 +0300

    Implemented KindleConverter

---
+++ src/Kindlegen/KindleConvertResult.cs
@@ -0,0 +1,9 @@
+namespace Kindlegen
+{
+    public class KindleConvertResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Output { get; set; }
+        public string Details { get; set; }
+    }
+}
