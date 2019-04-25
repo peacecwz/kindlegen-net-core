95bc243817f27ac20dd1fbd3a636f2ebc7c00e7f
Baris Ceviz (peacecwz) <me@barisceviz.com>
2019-04-25 04:52:49 +0300

    Created test project

---
+++ tests/Kindlegen.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kindlegen.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
