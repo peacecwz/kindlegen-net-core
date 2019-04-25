ea484938e6dad1d5978c4a85e134e1b02d333eec
Baris Ceviz <me@barisceviz.com>
2019-04-25 07:07:17 +0300

    Move to models folder

--- src/Kindlegen/CompressionLevel.cs
+++
@@ -1,9 +0,0 @@
-namespace Kindlegen
-{
-    public enum CompressionLevel
-    {
-        NoCompression = 0,
-        StandartDOC = 1,
-        KindleHuffdic = 2
-    }
-}
--- src/Kindlegen/KindleConvertResult.cs
+++
@@ -1,9 +0,0 @@
-namespace Kindlegen
-{
-    public class KindleConvertResult
-    {
-        public bool IsSuccess { get; set; }
-        public string Output { get; set; }
-        public string Details { get; set; }
-    }
-}
